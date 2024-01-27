b4364a54f341b2cbdf208d8cc9210f2a83ebd8a8
Isaac Batista <isaacbatst@gmail.com>
2024-01-27 02:21:43 -0300

    :speech_balloon: Improve seo data

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,6 +1,3 @@
-import { Send } from 'lucide-react'
-import React from 'react'
-import { Button } from './ui/button'
 import { SheetDemo } from './SheetContact'
 
 type Props = {}
@@ -10,7 +7,7 @@ const Header = (props: Props) => {
     <header>
       <div className='p-5 lg:px-0 lg:py-5 flex justify-between'>
         <div>
-          <h1 className='text-2xl font-bold'>Cronograma de estudos</h1>
+          <h1 className='text-2xl font-bold'>Planejei</h1>
           <p className='text-gray-500 hidden lg:block'>Crie seu plano de estudos e organize melhor seu tempo.</p>
         </div>
         <div className='flex items-center'>
