798895dfe5ec22c1068bd2ec130a1f49da99be7b
Isaac Batista <isaacbatst@gmail.com>
2024-01-27 02:05:01 -0300

    :lipstick: Add contact drawer

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,21 @@
+import { Send } from 'lucide-react'
 import React from 'react'
+import { Button } from './ui/button'
+import { SheetDemo } from './SheetContact'
 
 type Props = {}
 
 const Header = (props: Props) => {
   return (
     <header>
-      <div className='p-5'>
-        <h1 className='text-2xl font-bold'>Plano de estudos</h1>
-        <p className='text-gray-500'>Crie seu plano de estudos e organize melhor seu tempo.</p>
+      <div className='p-5 lg:px-0 lg:py-5 flex justify-between'>
+        <div>
+          <h1 className='text-2xl font-bold'>Cronograma de estudos</h1>
+          <p className='text-gray-500 hidden lg:block'>Crie seu plano de estudos e organize melhor seu tempo.</p>
+        </div>
+        <div className='flex items-center'>
+          <SheetDemo />
+        </div>
       </div>
       <hr />
     </header>
---
+++ src/components/SheetContact.tsx
@@ -0,0 +1,68 @@
+import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
+import { Label } from "@/components/ui/label"
+import {
+  Sheet,
+  SheetClose,
+  SheetContent,
+  SheetDescription,
+  SheetFooter,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from "@/components/ui/sheet"
+import { Send } from "lucide-react"
+
+const links = [
+  {
+    label: 'E-mail',
+    value: 'mailto:isaacbatst@gmail.com',
+    valueLabel: 'isaacbatst@gmail.com'
+  },
+  {
+    label: 'Telegram',
+    value: 'https://t.me/isaacbatst',
+    valueLabel: 't.me/isaacbatst'
+  },
+  {
+    label: 'Github',
+    value: 'https://github.com/isaacbatst',
+    valueLabel: 'github.com/isaacbatst'
+  }
+]
+
+export function SheetDemo() {
+  return (
+    <Sheet>
+      <SheetTrigger asChild>
+        <Button variant='ghost'>
+          <span className='mr-3'>Me contate</span> <Send/>
+        </Button>
+      </SheetTrigger>
+      <SheetContent>
+        <SheetHeader>
+          <SheetTitle>Isaac Batista</SheetTitle>
+          <SheetDescription>
+            @isaacbatst
+          </SheetDescription>
+        </SheetHeader>
+        <div className="grid gap-4 py-4">
+          <div className="flex flex-col">
+            {links.map((link) => (
+              <div key={link.value} className="flex justify-between items-center">
+                <Label htmlFor={link.value}>
+                  {link.label}
+                </Label>
+                <a href={link.value} id={link.value}>
+                  <Button variant='link' className='col-span-3'>
+                    {link.valueLabel}
+                  </Button>
+                </a>
+              </div> 
+            ))}
+          </div>
+        </div>
+      </SheetContent>
+    </Sheet>
+  )
+}
