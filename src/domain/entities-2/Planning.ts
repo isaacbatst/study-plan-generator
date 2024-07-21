2bcb8aeeedb6a9a8618500ba9f62036057cceb39
Isaac Batista <isaacbatst@gmail.com>
2024-07-21 13:02:02 -0300

    refactor: add distribution base

--- src/domain/entities-2/Planning.ts
+++ src/domain/entities-2/Planning.ts
@@ -1,8 +1,8 @@
-import { D, s } from "vitest/dist/reporters-rzC174PQ.js";
 import { Either } from "./either/Either";
 import { Subject } from "./Subject";
 import { StudyDay, StudyDayError } from "./StudyDay";
 import { PlanningStudyObject } from "./PlanningStudyObject";
+import { PlanningDistribution } from "./PlanningDistribution";
 
 type PlanningProps = {
   id: string;
@@ -10,7 +10,9 @@ type PlanningProps = {
   startDate: Date;
   subjects: Subject[];
   hoursPerDay: number;
+  distribution?: PlanningDistribution;
 };
+
 
 export class Planning {
   private constructor(
@@ -19,9 +21,10 @@ export class Planning {
     private startDate: Date,
     private subjects: Subject[],
     private hoursPerDay: number,
+    private distribution: PlanningDistribution = PlanningDistribution.UNTIL_FINISH_SUBJECT,
   ) {
   }
-
+  
   static create(props: PlanningProps): Either<string, Planning> {
     return Either.right(new Planning(
       props.id, 
@@ -29,26 +32,50 @@ export class Planning {
       props.startDate, 
       props.subjects, 
       props.hoursPerDay,
+      props.distribution,
     ));
   }
-
+  
   getId(): string {
     return this.id;
   }
-
+  
   getCreatedAt(): Date {
     return this.createdAt;
   }
-
+  
   getStartDate(): Date {
     return this.startDate;
   }
-
+  
   getSubjects(): Subject[] {
     return this.subjects;
   }
-
+  
+  getHoursPerDay(): number {
+    return this.hoursPerDay;
+  }
+  
   getStudyDays(): Either<string, StudyDay[]> {
+    switch(this.distribution) {
+    case PlanningDistribution.UNTIL_FINISH_SUBJECT:
+      return this.getStudyDaysUntilFinishSubject();
+    case PlanningDistribution.ALTERNATE_SUBJECT_PER_STUDY_OBJECT:
+      return Either.left('Not implemented');
+    case PlanningDistribution.ALTERNATE_SUBJECT_PER_DAY:
+      return Either.left('Not implemented');
+    case PlanningDistribution.ALTERNATE_SUBJECT_PER_WEEK:
+      return Either.left('Not implemented');
+    default:
+      return Either.left('Invalid distribution');
+    }
+  }
+
+  private getStudyDaysAlternatingSubjectPerStudyObject(): Either<string, StudyDay[]> {
+    return Either.left('Not implemented');
+  }
+  
+  private getStudyDaysUntilFinishSubject(): Either<string, StudyDay[]> {
     const studyDays: StudyDay[] = [];
     let currentStudyDayOrError = StudyDay.create({
       date: this.startDate,
@@ -59,7 +86,7 @@ export class Planning {
     }
     let currentStudyDay = currentStudyDayOrError.getRight();
     studyDays.push(currentStudyDay);
-
+    
     this.subjects.forEach((subject) => {
       subject.getStudyObjects().forEach((studyObject) => {
         const planningStudyObjectOrError = PlanningStudyObject.create({
@@ -82,11 +109,7 @@ export class Planning {
         }
       });
     })
-
+    
     return Either.right(studyDays);
-  }
-
-  getHoursPerDay(): number {
-    return this.hoursPerDay;
   }
 }
---
+++ src/domain/entities-2/PlanningDistribution.ts
@@ -0,0 +1,6 @@
+export enum PlanningDistribution {
+  UNTIL_FINISH_SUBJECT = 'until-finish-subject',
+  ALTERNATE_SUBJECT_PER_STUDY_OBJECT = 'alternate-subject-per-study-object',
+  ALTERNATE_SUBJECT_PER_DAY = 'alternate-subject-per-day',
+  ALTERNATE_SUBJECT_PER_WEEK = 'alternate-subject-per-week',
+}
