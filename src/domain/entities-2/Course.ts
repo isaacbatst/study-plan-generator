0e0b4e472d716fd2ede77b1d22bc9053addfb476
Isaac Batista <isaacbatst@gmail.com>
2024-07-26 22:09:11 -0300

    feat: courses entities

--- src/domain/entities-2/Course.ts
+++ src/domain/entities-2/Course.ts
@@ -1,13 +1,23 @@
+import { CoursePeriod } from "./CoursePeriod";
 import { Either } from "./either/Either";
+
+type CourseProps = {
+  id: string;
+  name: string;
+  periods: CoursePeriod[];
+};
 
 export class Course {
   private constructor(
     private id: string,
     private name: string,
+    private periods: CoursePeriod[] = [],
   ) {}
 
-  static create(id: string, name: string): Either<string, Course> {
-    return Either.right(new Course(id, name));
+  static create(
+    props: CourseProps,
+  ): Either<string, Course> {
+    return Either.right(new Course(props.id, props.name, props.periods));
   }
 
   getId(): string {
--- src/domain/entities-2/CoursePeriod.ts
+++ src/domain/entities-2/CoursePeriod.ts
@@ -1,3 +1,16 @@
+import { Either } from "./either/Either";
+import { Subject } from "./Subject";
+
 export class CoursePeriod {
-  
+  private constructor(
+    private subjects: Subject[]
+  ){}
+
+  static create(subjects: Subject[]): Either<string, CoursePeriod> {
+    return Either.right(new CoursePeriod(subjects))
+  }
+
+  getSubjects() {
+    return this.subjects
+  }
 } 
